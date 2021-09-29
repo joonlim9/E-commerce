472ccbb09f5c587a08bb093640051dc74deac391
joonlim9 <joonlim@umich.edu>
2021-09-29 15:38:47 -0400

    db connected, data modeling

--- backend/server.js
+++ backend/server.js
@@ -1,17 +1,16 @@
 import express from "express";
 import dotenv from "dotenv";
+import connectDB from "./config/db.js";
 import products from "./data/products.js";
 
 dotenv.config();
 
+connectDB();
 const app = express();
 
 app.get("/", (req, res) => {
   res.send("API is running....");
 });
-
-app.use(notFound);
-app.use(errorHandler);
 
 app.get("/api/products", (req, res) => {
   console.log("Do we have products?");
