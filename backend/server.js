a5efc97049fcfb886595e38e493b2c0a54840af8
joonlim9 <joonlim@umich.edu>
2021-09-29 23:02:40 -0400

    now fetching data from mongodb

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,9 @@
 import express from "express";
 import dotenv from "dotenv";
+import colors from "colors";
 import connectDB from "./config/db.js";
-import products from "./data/products.js";
-
+import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
+import productRoutes from "./routes/productRoutes.js";
 dotenv.config();
 
 connectDB();
@@ -12,22 +13,15 @@ app.get("/", (req, res) => {
   res.send("API is running....");
 });
 
-app.get("/api/products", (req, res) => {
-  console.log("Do we have products?");
-  console.log(products);
-  res.header("Access-Control-Allow-Origin", "*");
-  res.json(products);
-});
-
-app.get("/api/products/:id", (req, res) => {
-  const product = products.find((p) => p._id === req.params.id);
-  res.header("Access-Control-Allow-Origin", "*");
-  res.json(product);
-});
+app.use("/api/products", productRoutes);
+app.use(notFound);
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
 
 app.listen(
   PORT,
-  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+  console.log(
+    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
+  )
 );
