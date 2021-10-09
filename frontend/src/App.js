3e579f72a9b203bb0ad72e4304dc7b3f32c9ecdf
joonlim9 <joonlim@umich.edu>
2021-10-09 17:43:27 -0400

    searchbox on header

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 import { Container } from "react-bootstrap";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import SearchBox from "./components/SearchBox";
 import HomeScreen from "./screens/HomeScreen";
 import ProductScreen from "./screens/ProductScreen";
 import CartScreen from "./screens/CartScreen";
@@ -43,6 +44,7 @@ const App = () => {
             component={ProductListScreen}
             exact
           />
+          <Route path='/search/:keyword' component={HomeScreen} exact />
           <Route path='/' component={HomeScreen} exact />
         </Container>
       </main>
